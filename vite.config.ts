4a21435535899e7e1343b22dacf28ded681a8ed6
Rafal Koziarz <rkoz1.development@gmail.com>
2025-06-30 11:11:58 +0800

    Config update

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: 'https://cohort.hk/',
+  base: '/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
